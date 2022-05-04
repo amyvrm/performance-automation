9e7266f986a995452a2e8ab4c7692457f863798f
Amit Verma <amyvrm@gmail.com>
2022-05-04 19:24:54 +0530

    environment.sh appended package

--- iac_src/linux_agent.tf
+++ iac_src/linux_agent.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "performance_auto_machine" {
-	#ubuntu 22.04
-	ami = "ami-09d56f8956ab235b3"
-	#ubuntu 20.04
-	#ami                         = "ami-0c4f7023847b90238"
+	# ubuntu 22.04
+#	ami = "ami-09d56f8956ab235b3"
+	# ubuntu 20.04
+	ami                         = "ami-0c4f7023847b90238"
 	instance_type               = var.instance_type
 	key_name                    = var.key_name
 	associate_public_ip_address = "true"
@@ -37,9 +37,9 @@ resource "null_resource" "run_automation" {
 	provisioner "remote-exec" {
 		inline = [
 			"chmod +x /tmp/src/environment.sh",
-			"sudo /bin/bash /tmp/src/environment.sh",
-			"cd /tmp/",
-			"python src/perform_scenario.py --access_key ${var.access_key} --secret_key ${var.secret_key} --machine_info ${var.machine_file} --dsm_version ${var.dsmVersion} --stats ${var.stats} --graph ${var.graph} --path ${var.dsru_path} --nexus_uname ${var.nexus_user} --nexus_pwd ${var.nexus_pass} --scenario ${var.scenario}"
+#			"sudo /bin/bash /tmp/src/environment.sh",
+#			"cd /tmp/",
+#			"python src/perform_scenario.py --access_key ${var.access_key} --secret_key ${var.secret_key} --machine_info ${var.machine_file} --dsm_version ${var.dsmVersion} --stats ${var.stats} --graph ${var.graph} --path ${var.dsru_path} --nexus_uname ${var.nexus_user} --nexus_pwd ${var.nexus_pass} --scenario ${var.scenario}"
 		]
 	}
 	depends_on = [aws_instance.performance_auto_machine]
