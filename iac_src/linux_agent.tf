0e5c8554a4d9b565f28c02a3adfd91fe7e5f8d10
Amit Verma <amyvrm@gmail.com>
2022-05-04 19:43:03 +0530

    terraform python3 change

--- iac_src/linux_agent.tf
+++ iac_src/linux_agent.tf
@@ -40,7 +40,7 @@ resource "null_resource" "run_automation" {
 			"sleep 60",
 			"sudo /bin/bash /tmp/src/environment.sh",
 			"cd /tmp/",
-			"python src/perform_scenario.py --access_key ${var.access_key} --secret_key ${var.secret_key} --machine_info ${var.machine_file} --dsm_version ${var.dsmVersion} --stats ${var.stats} --graph ${var.graph} --path ${var.dsru_path} --nexus_uname ${var.nexus_user} --nexus_pwd ${var.nexus_pass} --scenario ${var.scenario}"
+			"python3 src/perform_scenario.py --access_key ${var.access_key} --secret_key ${var.secret_key} --machine_info ${var.machine_file} --dsm_version ${var.dsmVersion} --stats ${var.stats} --graph ${var.graph} --path ${var.dsru_path} --nexus_uname ${var.nexus_user} --nexus_pwd ${var.nexus_pass} --scenario ${var.scenario}"
 		]
 	}
 	depends_on = [aws_instance.performance_auto_machine]
