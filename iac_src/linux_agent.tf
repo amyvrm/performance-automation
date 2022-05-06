8297c44dde34b13fcf1a9393a42bec8a34689236
Amit Verma <amyvrm@gmail.com>
2022-05-06 18:37:05 +0530

    add quotes between teams webhook

--- iac_src/linux_agent.tf
+++ iac_src/linux_agent.tf
@@ -54,7 +54,7 @@ resource "null_resource" "run_automation" {
 			"chmod +x /tmp/src/environment.sh",
 			"sudo /bin/bash /tmp/src/environment.sh",
 			"cd /tmp/",
-			"python3 src/perform_scenario.py --access_key ${var.access_key} --secret_key ${var.secret_key} --machine_info ${var.local_manifest_file} --dsm_version ${var.dsmVersion} --stats ${var.stats} --graph ${var.graph} --path ${var.dsru_path} --nexus_url ${var.nexus_url} --nexus_uname ${var.nexus_user} --nexus_pwd ${var.nexus_pass} --scenario ${var.scenario} --webhook ${var.webhook} --jenkins_url ${var.jenkins_url} --build_user ${var.build_user}"
+			"python3 src/perform_scenario.py --access_key ${var.access_key} --secret_key ${var.secret_key} --machine_info ${var.local_manifest_file} --dsm_version ${var.dsmVersion} --stats ${var.stats} --graph ${var.graph} --path ${var.dsru_path} --nexus_url ${var.nexus_url} --nexus_uname ${var.nexus_user} --nexus_pwd ${var.nexus_pass} --scenario ${var.scenario} --webhook '${var.webhook}' --jenkins_url ${var.jenkins_url} --build_user ${var.build_user}"
 		]
 	}
 	depends_on = [aws_instance.performance_auto_machine]
