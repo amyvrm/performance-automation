37293d466a06ad851723476d461b44f835e92d48
Amit Verma <amyvrm@gmail.com>
2022-05-06 18:09:23 +0530

    Updated openssh installtion cmd

--- processzone/tools/install_openssh.ps1
+++ processzone/tools/install_openssh.ps1
@@ -1,7 +1,8 @@
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 Start-Sleep -s 5
-choco install openssh --version=0.0.19.0 --force --no-progress -y
+choco install openssh -y -f
+#choco install openssh --version=0.0.19.0 --force --no-progress -y
 Start-Sleep -s 5
 Set-Service -Name ssh-agent -StartupType ‘Automatic’
 Set-Service -Name sshd -StartupType ‘Automatic’
