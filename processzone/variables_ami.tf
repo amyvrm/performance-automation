664b81468ae51f63df8757ff635a144f060731c4
Amit Verma <amyvrm@gmail.com>
2022-08-17 16:48:40 +0530

    Changed windows ami id

--- processzone/variables_ami.tf
+++ processzone/variables_ami.tf
@@ -3,7 +3,8 @@ variable "red_hat_ami"{
 }
 
 variable "dsm_windows_ami"{
-	default = "ami-02c4947e847caf22c"
+#	default = "ami-02c4947e847caf22c"
+	default = "ami-0c95d38b24a19de18"
 }
 
 // working ami id: ami-042ac4e08bdc42efe
@@ -11,6 +12,7 @@ variable "dsm_windows_ami"{
 variable "dsa_windows_ami"{
 //	default = "ami-00dd0737502449ce1"
 //	default = "ami-0cdfa905439f4b9ca"
-	default = "ami-08ed5c5dd62794ec0"
+#	default = "ami-08ed5c5dd62794ec0"
+	default = "ami-0c95d38b24a19de18"
 }
 
