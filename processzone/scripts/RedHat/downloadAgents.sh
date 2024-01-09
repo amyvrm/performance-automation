c8e744564f71a2aded22a42eb9dc412d50063a06
Amit Verma <amyvrm@gmail.com>
2024-01-09 22:01:26 +0530

    installed pre-requisite package for zeep package

--- processzone/scripts/RedHat/downloadAgents.sh
+++ processzone/scripts/RedHat/downloadAgents.sh
@@ -21,7 +21,9 @@ cd /tmp
 
 #python3.7 -m pip install zeep
 
-#sudo pip3 install zeep
-pip3 install zeep
+sudo yum install -y libxml2-devel libxslt-devel python3.6-devel gcc
+sudo pip3 install --upgrade pip setuptools
+sudo pip3 install lxml
+sudo pip3 install zeep
 
 python3 uploadDSAToDSM.py --dsmHost 127.0.0.1 --dsmUser supermasteradmin --dsmPasswd OrangePlant#1980 --agentFolder /tmp/AgentPackages/
