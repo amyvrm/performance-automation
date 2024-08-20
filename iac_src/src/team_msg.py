8ecc1e4c7770676d5362df33c62db9268fe4e54f
Amit Verma <amyvrm@gmail.com>
2024-08-20 15:38:34 +0200

    Update team_msg.py

--- iac_src/src/team_msg.py
+++ iac_src/src/team_msg.py
@@ -47,6 +47,7 @@ def send_teams_notification(webhook, jenkins_url, build_user, scenario, stats_ur
 
     headers = {'content-type': 'application/json'}
     try:
+        print(json.dumps(message, indent=2))
         response = requests.post(webhook, data=json.dumps(message), headers=headers)
         response.raise_for_status()
         print("Team notification sent successfully")
