fc9b49dcb64c00ece268035900bdc3e6728ecb70
Amit Verma <amyvrm@gmail.com>
2025-02-27 13:04:11 -0500

    Update install_openssh.ps1

--- processzone/tools/install_openssh.ps1
+++ processzone/tools/install_openssh.ps1
@@ -4,7 +4,7 @@ Start-Sleep -s 5
 choco install openssh -y -f
 #choco install openssh --version=0.0.19.0 --force --no-progress -y
 Start-Sleep -s 5
-Set-Service -Name ssh-agent -StartupType ‘Automatic’
-Set-Service -Name sshd -StartupType ‘Automatic’
+Set-Service -Name ssh-agent -StartupType Automatic
+Set-Service -Name sshd -StartupType Automatic
 Start-Service ssh-agent
 Start-Service sshd
